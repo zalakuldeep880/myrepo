79b27865c894c9946aff05df9fba32a216f08211
zalakuldeep880 <107462075+zalakuldeep880@users.noreply.github.com>
2022-06-28 19:45:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ print("Bot Deployed Successfully!!!")
 async def start(bot, message):
     await bot.send_photo(message.chat.id, "https://telegra.ph/file/2a4d7c7ad8ac60603e4c7.jpg", caption="""me vansh ki gf zala ka loda chus ke aayi umm shalam aali """)
     await bot.send_photo(message.chat.id, "https://telegra.ph/file/f3eac8fb512bbd829c340.jpg", caption=""" ZALA EK HI ASLI MARD HE JO MERI CHUT KO THAND DILWYA VANSH KA TO NUNU HE NUNU """)
-    await bot.send_photo(message.chat.id, "https://telegra.ph/file/7f99886ff1df58e39c717.jpg", caption=""" VANSH KA NUNU DEKHN KE THAK GYI AAB ZALA KA LODA LUNGI 🔥 """
-    await bot.send_photo(message.chat.id, "https://telegra.ph/file/28acfc0f73dc5cf4e83d0.jpg", caption=""" +919718022068 YE LO MERA NUMBER AUR YE DM KARLO FREE SERVICE DUNGI :- @GoDDeSS_xD AGAR ME APNA USER NSME PE CHANNEL BANA LI TO SAMAJ JANA ZALA NE MERA KHANDAN CHOD DIYA AUR ME DAR GYI AUR 10000 BAAPO KI EKLOTI AULAD MERI MA 10000 LOGO SE CHUDWA KE MUJE PAIDA KI  HOGI 🥺🥺 ZALA OP VANSH ZALA KE LODE KI TOPI🔥🥺  🔥 """)
-
+    await bot.send_photo(message.chat.id, "https://telegra.ph/file/7f99886ff1df58e39c717.jpg", caption=""" VANSH KA NUNU DEKHN KE THAK GYI AAB ZALA KA LODA LUNGI 🔥 """)
+    await bot.send_photo(message.chat.id, "https://telegra.ph/file/28acfc0f73dc5cf4e83d0.jpg", caption=""" +919718022068 YE LO MERA NUMBER AUR YE DM KARLO FREE SERVICE DUNGI :- @GoDDeSS_xD AGAR ME APNA USER NSME PE CHANNEL BANA LI TO SAMAJ JANA ZALA NE MERA KHANDAN CHOD DIYA AUR ME DAR GYI AUR 10000 BAAPO KI EKLOTI AULAD MERI MA 10000 LOGO SE CHUDWA KE MUJE PAIDA KI  HOGI 🥺🥺 ZALA OP VANSH ZALA KE LODE KI TOPI🔥🥺🔥 """)
+                                                                                                                    
 
 
 bot.run()
